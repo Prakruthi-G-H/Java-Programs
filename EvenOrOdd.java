e9e9d561e1bc53a5f20f472ddc131fe7a858b263
Prakruthi G H <147101194+Prakruthi0@users.noreply.github.com>
2024-01-26 19:05:04 +0530

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,23 @@
+package programs;
+import java.util.*;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+	Scanner sc =new Scanner(System.in);
+	
+	while(true) {
+	System.out.println("Enter a number to check  Even or Odd");
+	int num=sc.nextInt();
+	if(num%2==0) {
+		System.out.println(num+" is Even Number");
+	}
+	else
+	{
+		System.out.println(num+" is Odd Number");
+	}
+
+	}
+
+}
+}
---
+++ SumOfTwoNumber.java
@@ -0,0 +1,17 @@
+package programs;
+import java.util.Scanner;
+
+public class SumOfTwoNumber {
+
+	public static void main(String[] args) {
+		int x, y, z;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter value of x ");
+		x = in.nextInt();
+		System.out.println("Enter value of y ");
+		y = in.nextInt();
+		z = x + y;
+		System.out.println("Sum of two numbers  is "+z);
+		}
+}
+
