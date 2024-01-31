be0a25c93fc902bce40d663c32b670202e6b799d
Prakruthi G H <147101194+Prakruthi0@users.noreply.github.com>
2024-01-31 14:48:00 +0530

    Add files via upload

---
+++ Pattern/Pattern4.java
@@ -0,0 +1,18 @@
+package pattern;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i =n;i>0;i--)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
