02848dbd59482209650d17d18d5e28eba0e4502f
Prakruthi G H <147101194+Prakruthi0@users.noreply.github.com>
2024-02-06 11:11:57 +0530

    Add files via upload

---
+++ Basic Programs/MultiplicationOfNumber.java
@@ -0,0 +1,20 @@
+package programs;
+
+import java.util.Scanner;
+
+public class MultiplicationOfNumber {
+
+	public static void main(String[] args) {
+		int x, y, z;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter value of x ");
+		x = in.nextInt();
+		System.out.println("Enter value of y ");
+		y = in.nextInt();
+		z = x * y;
+		System.out.println("Sum of two numbers  is "+z);
+		in.close();
+
+	}
+
+}
