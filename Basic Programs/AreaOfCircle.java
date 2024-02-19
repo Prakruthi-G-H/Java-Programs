b12dc9199f0d93f66a94cef0421b3a47811de393
Prakruthi G H <147101194+Prakruthi0@users.noreply.github.com>
2024-02-19 12:14:46 +0530

    Add files via upload

---
+++ Basic Programs/AreaOfCircle.java
@@ -0,0 +1,21 @@
+package programs;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		int radius;
+		float area;
+		float PI=3.14f;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a Radius of the CIrcle");
+		radius=s.nextInt();
+		
+		area=PI*radius;
+		System.out.println("Area Of Circle is "+area);
+       s.close();
+
+	}
+
+}
