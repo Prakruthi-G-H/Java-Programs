41cb6af9957817f3f6b9d2522f0aeaa2bf6f05df
Prakruthi G H <147101194+Prakruthi0@users.noreply.github.com>
2024-02-20 12:33:27 +0530

    Add files via upload

---
+++ Basic Programs/LoginValidation.java
@@ -0,0 +1,24 @@
+package programs;
+
+
+public class LoginValidation {
+
+	public static void main(String[] args) {
+		String id = "Admin";
+		int password=2158235;
+		if (id=="Admin") {
+			System.out.println("User id is Valid");
+			if (password==21569445) {
+				System.out.println("User Password is Valid");
+				System.out.println("Login Successfull...");
+			} else {
+            System.out.println("User Password is Invalid");
+            System.out.println("Login Unsuccessfulll");
+			}
+		} else {
+          System.out.println("User id is Invalid");
+		}
+         
+	}
+
+}
