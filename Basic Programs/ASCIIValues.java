ec664dc060ae927eb4ae9f649069d380a20f36d5
Prakruthi G H <147101194+Prakruthi0@users.noreply.github.com>
2024-02-20 12:30:22 +0530

    Add files via upload

---
+++ Basic Programs/ASCIIValues.java
@@ -0,0 +1,16 @@
+package programs;
+
+import java.util.Scanner;
+
+public class ASCIIValues {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+	      System.out.println("Enter a any character");
+	      char c=s.next().charAt(0);
+	      int a=c;
+	      System.out.println("ASCII Values of "+ c + "is :"+a);
+     s.close();
+	}
+
+}
