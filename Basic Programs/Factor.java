01fda699d8c5031dbeab20e66f49e4b69ce79abc
Prakruthi G H <147101194+Prakruthi0@users.noreply.github.com>
2024-02-21 21:56:18 +0530

    Add files via upload

---
+++ Basic Programs/Factor.java
@@ -0,0 +1,21 @@
+package programs;
+
+import java.util.Scanner;
+
+public class Factor {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number");
+		 int n=s.nextInt();
+		 for (int i = 1; i <= n; i++) {
+			if (n%i==0) {
+				System.out.print(i+" ");
+			}
+		}
+		 System.out.print( " are Factor of "+n);
+		s.close();
+
+	}
+
+}
